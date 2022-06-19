094fe967b1357455b65cb31cf1e9f79486ed381c
anden1305 <andreasbigom@gmail.com>
2022-06-19 21:19:59 +0200

    Clean up of repo

--- test_env.py
+++ test_env.py
@@ -1,14 +1,2 @@
-from matplotlib.pyplot import pie
+
 from AAM import AA
-import numpy as np
-
-AAM = AA()
-
-AAM.load_analysis("K=4 - RBOAA", "RBOAA",False)
-AAM._results["RBOAA"][0].N = len(AAM._results["RBOAA"][0].X[0,:])
-AAM.plot("RBOAA","barplot_all")
-
-
-for i in range(4):
-    AAM.plot("RBOAA","barplot",archetype_number=i+1)
-    
